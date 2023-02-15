8db52f85183a3f90b7113ccafe1b6972f38cdebc
António <antonio_mourao@hotmail.com>
2023-02-15 11:51:30 +0000

    initial setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,36 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
-describe('AppComponent', () => {
+describe('Component Unit Testing', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent, NxWelcomeComponent, RouterTestingModule],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(async () => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  it(`should have as title 'wait-for-async'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('wait-for-async');
+  it('RQ01 - should create the app', () => {
+    expect(component).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it(`RQ02 - should have as title 'wait-for-async'`, () => {
+    expect(component.title).toEqual('wait-for-async');
+  });
+
+  it('RQ03 - should render title', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wait-for-async';
+
+  value = 0;
+  message!: string;
+
+  increment() {
+    if (this.value < 15) {
+      this.value += 1;
+      this.message = '';
+    } else {
+      this.message = 'Maximum reached!';
+    }
+  }
+
+  decrement() {
+    if (this.value > 0) {
+      this.value -= 1;
+      this.message = '';
+    } else {
+      this.message = 'Minimum reached!';
+    }
+  }
 }
